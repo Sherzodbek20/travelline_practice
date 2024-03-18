70932ac7e9da331a082cf6da5aa97ad1f4b5a2dc
Sherzodbek20 <159790504+Sherzodbek20@users.noreply.github.com>
2024-03-19 02:13:37 +0300

    Add files via upload

---
+++ lecture1(14.03)/маркет1/маркет1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+        Console.WriteLine("Введите название товара:");
+        string product = Console.ReadLine();
+
+        Console.WriteLine("Введите количество товара:");
+        int count = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите адрес доставки:");
+        string address = Console.ReadLine();
+
+        Console.WriteLine($"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно? (Да/Нет)");
+        string confirmation = Console.ReadLine();
+
+        if (confirmation.ToLower() == "да")
+        {
+            Console.WriteLine($"{name}! Ваш заказ {product} в количестве {count} оформлен!");
+            DateTime deliveryDate = DateTime.Today.AddDays(3);
+            Console.WriteLine($"Ожидайте доставку по адресу {address} к {deliveryDate.ToString("dd/MM/yyyy")}");
+        }
+        else
+        {
+            Console.WriteLine("Пожалуйста, повторите ввод данных.");
+        }
