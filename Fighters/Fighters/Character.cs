a58f6eeaad314631d24448fabccaa98387aa1098
Sherzodbek20 <159790504+Sherzodbek20@users.noreply.github.com>
2024-04-02 01:14:05 +0300

    Add files via upload

---
+++ Fighters/Fighters/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Character
+{
+    public string Name { get; private set; }
+    private string weapon;
+    private string armor;
+    private Random random = new Random();
+
+    public int Health { get; private set; } = 100;
+
+    public Character(string name, string weapon, string armor)
+    {
+        this.Name = name;
+        this.weapon = weapon;
+        this.armor = armor;
+    }
+
+    public int Attack()
+    {
+        double damageModifier = random.NextDouble() * 0.3 - 0.2;
+
+        return (int)(10 * (1 + damageModifier));
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Console.WriteLine($"{this.Name} получает {damage} урона.");
+        if (Health <= 0)
+        {
+            Console.WriteLine($"{this.Name} погибает...");
+        }
+    }
+
+    public bool IsAlive()
+    {
+        return Health > 0;
+    }
+}
---
+++ Fighters/Fighters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BattleGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Приложение: Введите команду (для выхода введите 'exit')");
+                Console.WriteLine("Добавить нового бойца на арену - 1");
+                Console.WriteLine("Начать битву - 2");
+                string command = Console.ReadLine();
+
+                if (command == "exit")
+                {
+                    break;
+                }
+
+                switch (command)
+                {
+                    case "1":
+                        Fighters.AddFighter();
+                        break;
+                    case "2":
+                        Fighters.StartBattle();
+                        break;
+                    default:
+                        Console.WriteLine("Некорректная команда. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
